878f7d281a90d3552aecfdc4e6cd2a91e26402f4
jpardobl <jpardo@digitalhigh.es>
2014-01-26 18:57:23 +0100

    Change release number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name = 'hautomation-restclient',
-    version = '0.2.1',
+    version = '0.3',
     packages = ['hautomation_restclient'],
     include_package_data = True,
     license = 'BSD License',
